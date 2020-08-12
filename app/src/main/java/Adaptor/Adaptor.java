7091d0decdf7bcffd3df812c0e88d7e124e2ab1c
DarksterTwilight <avisingh19671967@gmail.com>
2020-08-12 19:05:05 +0530

    test2

---
+++ app/src/main/java/Adaptor/Adaptor.java
@@ -0,0 +1,29 @@
+package Adaptor;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adaptor extends RecyclerView.Adapter {
+    private Context context;
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    //   private List<>
+
+}
